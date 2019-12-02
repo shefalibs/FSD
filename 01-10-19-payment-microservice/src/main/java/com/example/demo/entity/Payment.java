5a36ff67322d2cad3b0c4990ed62d1c245a88c3d
Shefali BS <shefalibs.09@gmail.com>
2019-12-02 18:03:04 +0530

    Payment

---
+++ 01-10-19-payment-microservice/src/main/java/com/example/demo/entity/Payment.java
@@ -0,0 +1,58 @@
+package com.example.demo.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.example.demo.util.PaymentMode;
+import com.example.demo.util.PaymentStatus;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Payment")
+public class Payment {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column
+    private String email;
+    @Column
+    private String name;
+    @Column
+    private String phone;
+    @Column
+    private String productInfo;
+    @Column
+    private Double amount;
+    @Column
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus paymentStatus;
+    @Column
+    @Temporal(TemporalType.DATE)
+    private Date paymentDate;
+    @Column
+    private String txnId;
+    @Column
+    private String mihpayId;
+    @Column
+    @Enumerated(EnumType.STRING)
+    private PaymentMode mode;
+    
+}
